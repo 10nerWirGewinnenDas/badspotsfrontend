a422e1c5a7319c6829e3ce981ece24d4f535e805
David Brandes <brandes.1@campus.tu-berlin.de>
2024-03-29 23:15:54 +0100

    npm dedupe and fix

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,39 +1,44 @@
-import React, { useRef } from 'react';
+import React, { Suspense, lazy, useRef } from 'react';
 
 import './Map.css';
 import Map, {
-	LngLatBoundsLike,
-	LngLatLike,
-	MapRef,
+    LngLatBoundsLike,
+    LngLatLike,
+    MapRef,
 } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import MarkerContainer from './Markers/MarkerContainer';
-import LocationMarker from './Markers/Classes/LocationMarker/LocationMarker';
+
+const MarkerContainer = lazy(() => import('./Markers/MarkerContainer'));
+const LocationMarker = lazy(() => import('./Markers/Classes/LocationMarker/LocationMarker'));
 
 interface MapProps {
-	formSubmitted: boolean;
-	userPosition: {lng: number, lat: number} | null | null;
-	setUserPosition: (position: {lng: number, lat: number} | null) => void;
+    formSubmitted: boolean;
+    userPosition: { lng: number, lat: number } | null | null;
+    setUserPosition: (position: { lng: number, lat: number } | null) => void;
     isFirstOpen: boolean;
 }
 
-export const berlinViewPosition: {lng: number, lat: number} = {lng: 13.388, lat: 52.5162};
+export const berlinViewPosition: { lng: number, lat: number } = { lng: 13.388, lat: 52.5162 };
+
 
 const FreifahrenMap: React.FC<MapProps> = ({
-	formSubmitted,
-	userPosition,
-	setUserPosition,
+    formSubmitted,
+    userPosition,
+    setUserPosition,
     isFirstOpen
 }) => {
-    const sw: LngLatLike = {lng: 12.8364646484805, lat: 52.23115511676795}
-    const ne: LngLatLike = {lng: 13.88044556529124, lat: 52.77063424239867}
+    const sw: LngLatLike = { lng: 12.8364646484805, lat: 52.23115511676795 }
+    const ne: LngLatLike = { lng: 13.88044556529124, lat: 52.77063424239867 }
 
-	const maxBounds: LngLatBoundsLike = [sw, ne];
+    const maxBounds: LngLatBoundsLike = [sw, ne];
 
     const map = useRef<MapRef>(null);
-	return (
+
+   
+    return (
         <div id='map-container' data-testid='map-container'>
             <Map
+                reuseMaps
                 data-testid='map'
                 ref={map}
                 id='map'
@@ -45,16 +50,16 @@ const FreifahrenMap: React.FC<MapProps> = ({
                 maxZoom={14}
                 minZoom={10}
                 maxBounds={maxBounds}
-
-                mapStyle={`https://api.jawg.io/styles/jawg-streets.json?access-token=${process.env.REACT_APP_JAWG_ACCESS_TOKEN}`}
+                
+                mapStyle={`https://api.jawg.io/styles/f3354f40-2334-41b6-a537-c72decb830b2.json?access-token=${process.env.REACT_APP_JAWG_ACCESS_TOKEN}`}
             >
-
-               {!isFirstOpen && <LocationMarker userPosition={userPosition} setUserPosition={setUserPosition}/>}
-               <MarkerContainer isFirstOpen={isFirstOpen} formSubmitted={formSubmitted} />
-
+                <Suspense fallback={<div>Loading...</div>}>
+                    {!isFirstOpen && <LocationMarker userPosition={userPosition} setUserPosition={setUserPosition} />}
+                    <MarkerContainer isFirstOpen={isFirstOpen} formSubmitted={formSubmitted} />
+                </Suspense>
             </Map>
         </div>
-	);
+    );
 };
 
 export default FreifahrenMap;
