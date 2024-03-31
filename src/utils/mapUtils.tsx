858ba39c56cd8ca7ec985b29db598fcacfff1c5f
David Brandes <brandes.1@campus.tu-berlin.de>
2024-04-01 01:18:42 +0200

    weird format

--- src/utils/mapUtils.tsx
+++ src/utils/mapUtils.tsx
@@ -10,9 +10,9 @@ export function calculateDistance(
 	const a =
 		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
 		Math.cos(deg2rad(lat1)) *
-			Math.cos(deg2rad(lat2)) *
-			Math.sin(dLon / 2) *
-			Math.sin(dLon / 2);
+		Math.cos(deg2rad(lat2)) *
+		Math.sin(dLon / 2) *
+		Math.sin(dLon / 2);
 	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 	const distance = R * c; // Distance in km
 	return distance;
