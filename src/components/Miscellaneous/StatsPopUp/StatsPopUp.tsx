0b243d5dfdbaf2f2e728ee1a8c30fffb6f1adb86
Johan Trieloff <johan@trieloff.net>
2024-04-04 17:12:27 +0200

    added animation

--- src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
+++ src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './StatsPopUp.css';
 
@@ -8,14 +8,20 @@ interface StatsPopUpProps {
 
 const StatsPopUp: React.FC<StatsPopUpProps> = ({ className }) => {
     const [message, setMessage] = useState<string>('<p><strong>ca. 26000 meldende</strong><br /> in Berlin</p>');
+    const [popOut, setPopOut] = useState<boolean>(false);
 
     // reset message after 5 seconds
     setTimeout(() => {
         setMessage('<p><strong>500 Meldungen</strong><br /> heute in Berlin</p>');
-    }, 5000);
+    }, 3.5*1000);
+
+    useEffect(() => {
+        setPopOut(true);
+        setTimeout(() => setPopOut(false), 500); // assuming the animation duration is 500ms
+    }, [message]);
 
     return (
-        <div className={`stats-popup ${className}`} dangerouslySetInnerHTML={{ __html: message }} />
+        <div className={`stats-popup ${className} ${popOut ? 'pop-out' : ''}`} dangerouslySetInnerHTML={{ __html: message }} />
     );
 }
 
