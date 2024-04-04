b12a75be73c1de6207d48a169a8a946508beff3b
Johan Trieloff <johan@trieloff.net>
2024-04-04 21:41:13 +0200

    fixed lint

--- src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
+++ src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { getNumberOfReportsInLast24Hours } from 'src/utils/dbUtils';
 import './StatsPopUp.css';
 
@@ -20,27 +20,27 @@ const StatsPopUp: React.FC<StatsPopUpProps> = ({ className }) => {
     setPopOut(true);
   };
 
-  const hidePopupAfterAnimation = () => {
+  const hidePopupAfterAnimation = useCallback(() => {
     setTimeout(() => {
       setPopOut(false);
       setTimeout(() => setIsVisible(false), timeForOneMessage);
     }, timeForPopOutAnimation);
-  };
+  }, [timeForOneMessage, timeForPopOutAnimation]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       updateMessageAndShowPopup().then(hidePopupAfterAnimation);
     }, timeForOneMessage);
-
+  
     return () => clearTimeout(timer);
-  }, []);
-
+  }, [hidePopupAfterAnimation, timeForOneMessage]);
+  
   useEffect(() => {
     if (popOut) {
       const timer = setTimeout(() => setPopOut(false), timeForPopOutAnimation);
       return () => clearTimeout(timer);
     }
-  }, [popOut]);
+  }, [popOut, timeForPopOutAnimation]);
 
   return (
     <div className={`
