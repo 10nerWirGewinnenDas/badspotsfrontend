abfc51b6cf94b9fa6be3addcd1ca7e83e47e26a3
Johan Trieloff <johan@trieloff.net>
2024-04-04 17:46:13 +0200

    fade out

--- src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
+++ src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
@@ -7,21 +7,35 @@ interface StatsPopUpProps {
 }
 
 const StatsPopUp: React.FC<StatsPopUpProps> = ({ className }) => {
-    const [message, setMessage] = useState<string>('<p><strong>ca. 26000 meldende</strong><br /> in Berlin</p>');
-    const [popOut, setPopOut] = useState<boolean>(false);
-
-    // reset message after 5 seconds
-    setTimeout(() => {
-        setMessage('<p><strong>500 Meldungen</strong><br /> heute in Berlin</p>');
-    }, 3.5*1000);
+    const [message, setMessage] = useState('<p><strong>ca. 26000 meldende</strong><br /> in Berlin</p>');
+    const [popOut, setPopOut] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
 
     useEffect(() => {
-        setPopOut(true);
-        setTimeout(() => setPopOut(false), 500); // assuming the animation duration is 500ms
-    }, [message]);
+        const timer = setTimeout(() => {
+            setMessage('<p><strong>500 Meldungen</strong><br /> heute in Berlin</p>');
+            setPopOut(true);
+
+            setTimeout(() => {
+                setPopOut(false);
+                setTimeout(() => setIsVisible(false), 3500); // Start hiding after the second message is shown
+            }, 500); // This should match the duration of the pop-out animation
+        }, 3.5 * 1000);
+
+        // Cleanup function to clear timeout if component unmounts
+        return () => clearTimeout(timer);
+    }, []);
+
+    // Listen for popOut changes to apply pop-out animation on message change
+    useEffect(() => {
+        if (popOut) {
+            const timer = setTimeout(() => setPopOut(false), 500);
+            return () => clearTimeout(timer);
+        }
+    }, [popOut]);
 
     return (
-        <div className={`stats-popup ${className} ${popOut ? 'pop-out' : ''}`} dangerouslySetInnerHTML={{ __html: message }} />
+        <div className={`stats-popup ${className} ${popOut ? 'pop-out' : ''} ${!isVisible ? 'fade-out': ''}`}dangerouslySetInnerHTML={{ __html: message }} />
     );
 }
 
