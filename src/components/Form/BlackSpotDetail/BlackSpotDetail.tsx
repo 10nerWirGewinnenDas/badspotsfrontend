7a43068330dbe85beabd4336b449ac7b5610aef9
Johan Trieloff <johan@trieloff.net>
2024-04-06 11:15:49 +0200

    change color to red when upvoting

--- src/components/Form/BlackSpotDetail/BlackSpotDetail.tsx
+++ src/components/Form/BlackSpotDetail/BlackSpotDetail.tsx
@@ -97,7 +97,7 @@ const BlackSpotDetail: React.FC<ReportFormProps> = ({
 			<p>{spot!.description}</p>
 
 			<div className='votingSection'>
-			<svg onClick={handleUpvote} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 12l-10-10v7h-14v6h14v7z"/></svg>
+			<svg className={`${voted ? 'upvoted' : ''}`} onClick={handleUpvote} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 12l-10-10v7h-14v6h14v7z"  /></svg>
 			<p>{votes}</p>
 			</div>
 			<ul>
