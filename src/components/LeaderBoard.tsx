bdd62e7f7f481bf121cad0013a7c5156e1613dd4
Johan Trieloff <johan@trieloff.net>
2024-04-06 06:43:57 +0200

    added animation

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -12,9 +12,6 @@ const LeaderBoard: React.FC<LeaderBoardProps> = ({ leaderboard }) => {
 
     const toggleModal = () => setIsExpanded(!isExpanded);
 
-    // Assuming you're using the `votes` as the score.
-    // This assumes your LeaderElement component can accept the `score` as number.
-    // Adjust the logic here as necessary.
     const renderLeaderElement = (element: GetBlackSpotDto, position: number) => (
         <LeaderElement
             key={element.id}
@@ -25,23 +22,22 @@ const LeaderBoard: React.FC<LeaderBoardProps> = ({ leaderboard }) => {
     );
 
     return (
-        <div className='leaderboard'>
-            {/* Ensure leaderboard has at least one element before trying to access it */}
+        <div className={`leaderboard ${isExpanded ? 'expanded' : ''}`}>
             {leaderboard.length > 0 && renderLeaderElement(leaderboard[0], 1)}
             
+            {isExpanded && (
+                <div className='leaderboard-modal'>
+                    {leaderboard.slice(1).map((element, index) =>
+                        renderLeaderElement(element, index + 2)
+                    )}
+                </div>
+            )}
+
             <div onClick={toggleModal}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16" style={{ cursor: 'pointer' }}>
                     <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </div>
-
-            {isExpanded && (
-                <div className='leaderboard-modal'>
-                    {leaderboard.slice(1).map((element, index) =>
-                        renderLeaderElement(element, index + 2) // Adjust position since slice starts at 1
-                    )}
-                </div>
-            )}
         </div>
     );
 };
